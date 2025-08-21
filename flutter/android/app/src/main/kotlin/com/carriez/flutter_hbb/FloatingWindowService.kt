a4b19a6df5c95315129deacc169a634fe19ee5db
1527467845qqcom <120783449+1527467845qqcom@users.noreply.github.com>
2025-08-21 10:20:10 +0800

    Update FloatingWindowService.kt

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/FloatingWindowService.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/FloatingWindowService.kt
@@ -28,6 +28,13 @@ import android.widget.PopupMenu
 import com.caverock.androidsvg.SVG
 import ffi.FFI
 import kotlin.math.abs
+import android.widget.FrameLayout
+import android.widget.TextView
+import android.graphics.Color
+import android.view.Gravity
+import android.util.TypedValue
+import android.view.View
+import android.view.ViewGroup
 
 class FloatingWindowService : Service(), View.OnTouchListener {
 
